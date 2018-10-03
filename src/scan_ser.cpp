a9568f4fc2a014cc20a8318b1bee26da2d384ea7
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2018-10-03 15:44:16 +0800

    Update scan_ser.cpp

--- src/scan_ser.cpp
+++ src/scan_ser.cpp
@@ -1,3 +1,6 @@
+//xx
+//实现udp 下结构体数据接收
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
@@ -40,7 +43,6 @@ void handle_udp_msg(int fd)
     int count;
     struct sockaddr_in clent_addr;  //clent_addr用于记录发送方的地址信息
     while(ros::ok())
-  //  while(1)
     {
       //  memset(buf, 0, BUFF_LEN);
         len = sizeof(clent_addr);
@@ -58,10 +60,6 @@ void handle_udp_msg(int fd)
   //     std::cout<<"scan_data_buf:   "<<scan_data_buf.frame_id;
   //      std::cout<<scan_data_buf.angle_min<<"  "<<scan_data_buf.angle_max<<"   "<<scan_data_buf.scan_time<< std::endl;
         std::cout<<scan_data_buf.ranges[0]<<"  "<<scan_data_buf.intensities[1]<< std::endl;
-       // memset(buf, 0, BUFF_LEN);
-       // sprintf(buf, "I have recieved %d bytes data!\n", count);  //回复client
-       // printf("server:%s\n",buf);  //打印自己发送的信息给
-       // sendto(fd, buf, BUFF_LEN, 0, (struct sockaddr*)&clent_addr, len);  //发送信息给client，注意使用了clent_addr结构体指针
 
     }
 }
